e1d761669c83b9ecb8775947ef2c560e7a418bda
Hizbullah Watandost <watandost10@gmail.com>
2021-01-30 00:46:07 +0430

    First Commit

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# Welcome to FreeCodeGram
+
+This is a community of fello devopers and we are happy that you joined us.
+
+
+
+All The Best,<br>
+{{ config('app.name') }}
+@endcomponent
